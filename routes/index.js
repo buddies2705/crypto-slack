317381242714b841296e00dcb0447702208d4e81
uniquevik <uniquevik28@gmail.com>
2017-12-10 16:20:30 +0530

    changes to 24 hours and last hours dat

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ var router = express.Router();
 
 
 router.post('/', function(req, res) {
-    console.log(req.body)
-    getPrice(req.body.text , function (err , data){
-      res.send(createSuccessResponse(data));      
+  var coinName = req.body.text;
+    aggregateDataLast24Hours(coinName , function (err , data){
+      res.send(createSuccessResponseForLast24Data(data.Data ,coinName));      
     });
 });
 
-function createSuccessResponse(data){
+
+function createSuccessResponse(data ){
   var res = { 
     "text": "*BTC =* " + data.BTC  + " *EUR =* " + data.EUR + " *USD =* "+ data.USD,
     "username": "coinprice",
@@ -18,6 +19,60 @@ function createSuccessResponse(data){
   }
 return res;
 }
+
+
+function createSuccessResponseForLast24Data(data, coinName){
+var lastDayValue = calculatePriceChange(data,1460); 
+var lastHourValue = calculatePriceChange(data,60); 
+var currentValue =  data[data.length - 1].close;
+  var res = { 
+    "text": "*"+coinName.toUpperCase()+"*" +" - " +"*"+currentValue+"*"  + " *$*" +"\n *24H* = " 
+    + lastDayValue + "% " + getEmoji(lastDayValue)+ "\n *1H* = " + lastHourValue + "% " + getEmoji(lastHourValue),
+    "username": "coinprice",
+    "mrkdwn": true
+  }
+return res;
+}
+
+
+function getEmoji(value){
+  var emoji = ":smile:";
+  switch(true) {
+    case value>0:
+        emoji = ":smile:";
+        break;
+    case value<0:
+        emoji = ":upside_down_face:";
+        break;
+    default:
+        emoji=":smile:";
+}
+return emoji;
+}
+
+function calculatePriceChange(data , mimutes){
+  var end = data[data.length - 1].close;
+  var start = data[data.length - mimutes].close;
+  var priceChange = (1 - (start / end)) * 100
+  return priceChange.toFixed(2);
+}
+
+
+function createSuccessResponseForSnapShot(data){
+  var text  = createText(data);
+  var res = { 
+    "text": "*BTC =* " + data.BTC  + " *EUR =* " + data.EUR + " *USD =* "+ data.USD,
+    "username": "coinprice",
+    "mrkdwn": true
+  }
+return res;
+}
+
+
+function createText(data){
+
+}
+
 
 //price 
 function getPrice(coin,callback){
@@ -29,7 +84,6 @@ var fsym = "fsym";
   }
 coin = coin.toUpperCase();
 url = 'https://min-api.cryptocompare.com/data/'+api+'?'+fsym+'='+coin+'&tsyms=BTC,USD,EUR'
-console.log(url)
   rest.get(url).on('complete', function(data) {   
     console.log(data); // auto convert to object
     callback(null ,data) 
@@ -42,4 +96,23 @@ module.exports = router;
 
 
 
+function coinPriceSnapShot(coin,callback){
+coin = coin.toUpperCase();  
+url= 'https://www.cryptocompare.com/api/data/coinsnapshot/?fsym='+ coin +'&tsym=USD'
+console.log(url);
+rest.get(url).on('complete', function(data) {   
+  console.log(data); // auto convert to object
+  callback(null ,data) 
+});
+}
 
+
+
+function aggregateDataLast24Hours(coin,callback){
+  coin = coin.toUpperCase();  
+  url= 'https://min-api.cryptocompare.com/data/histominute?fsym='+coin+'&tsym=USD&limit=1460&aggregate=1&e=CCCAGG'
+  console.log(url);
+  rest.get(url).on('complete', function(data) {   
+    callback(null ,data) 
+  });
+  }
